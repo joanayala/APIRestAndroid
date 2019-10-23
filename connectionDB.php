b824313eacdebbc09c4b6a0005bce793ea7aa55d
joanayala <jflaco85@gmail.com>
2019-10-23 08:14:03 -0500

    Scripts APIRest Android APP

---
+++ connectionDB.php
@@ -0,0 +1,9 @@
+<?php
+    //Data base connections - MySQL credentials.
+    $servername = 'localhost'; //127.0.0.1 or staticIP-DinamicIP
+    $username = 'root';
+    $password = '';
+    $dbname = 'market';
+    $port = '3306';
+
+?>
